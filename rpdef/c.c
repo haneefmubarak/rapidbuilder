ab73d4c5b2a2f76a57016f7a0422d0578e913ea7
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-09-13 19:23:01 -0700

    Minor fixes: rpdef/c.c

--- rpdef/c.c
+++ rpdef/c.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 int whichCC (unsigned char *cc) {
-	char * whichCC = malloc (11 + strlen (cc)); // Allocate enough memory for "env which"...
-	strcpy (whichCC, "env which ");
+	char * whichCC = malloc (19 + strlen (cc)); // Allocate enough memory for "env which"...
+	strcpy (whichCC, "/usr/bin/env which ");
 	strcat (whichCC, cc); // Append the name of the compiler
 	return system (whichCC);
 }
