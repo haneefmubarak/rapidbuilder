600d42c8506c005b6d09a08ee51b12303c495134
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-09-13 19:35:04 -0700

    added STRUCT cflags in FILE c.h

--- rpdef/c.h
+++ rpdef/c.h
@@ -1,4 +1,14 @@
 // c.h -- header file for rpdef C
 
+typedef struct {
+	char *always;
+	char *native;
+	char *warn;
+	char *quiet;
+	char *debug;
+	char *lib;
+	char *link;
+} cflags;
+
 extern int whichCC (unsigned char *);
 
